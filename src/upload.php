22110ca010f345c179bfe8d88007e7e71833e1b7
Ekter <ektermoi1@gmail.com>
2022-06-03 14:11:17 +0200

    d

--- src/upload.php
+++ src/upload.php
@@ -43,6 +43,7 @@ if ($uploadOk == 0) {
 } else {
   try {
     echo "######/n";
+    echo $_FILES["fileToUpload"]["tmp_name"];
     $name_file=end(explode("/",$_FILES["fileToUpload"]["tmp_name"]));
     echo $name_file;
     $name_file=explode(".",$name_file);
