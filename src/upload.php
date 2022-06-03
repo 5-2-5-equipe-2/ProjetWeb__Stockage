82358e4abf63beb9757e2b275a44bc36fb04d033
Ekter <ektermoi1@gmail.com>
2022-06-03 12:45:06 +0200

    send image to the server

--- src/upload.php
+++ src/upload.php
@@ -1,8 +1,10 @@
 <?php
-$target_dir = "uploads/";
+$target_dir = "/var/www/html/images/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -14,4 +16,39 @@ if(isset($_POST["submit"])) {
     $uploadOk = 0;
   }
 }
-?>
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5000000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" && $imageFileType != "svg") {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  try {
+    $name_file=explode(".",$_FILES["fileToUpload"]["tmp_name"]);
+    $f =fopen($_FILES["fileToUpload"]["tmp_name"],"r");
+    // fpassthru($f);
+    $name_file = $name_file[0]+rand(1, 10000000000000)+".".$name_file[1];
+    $f2=fopen("uploads/$name_file","w");
+    fwrite($f2,fread($f,filesize($_FILES["fileToUpload"]["tmp_name"])));
+    echo "";
+  } catch (Exception $e) {
+    echo "Sorry, there was an error uploading your file.";
+  }}
