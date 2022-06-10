70be24ccad19483a3c8d4e8c47570c6996357300
Ekter <ektermoi1@gmail.com>
2022-06-10 18:33:49 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ça marche

--- src/uploadimage.php
+++ src/uploadimage.php
@@ -1,8 +1,13 @@
 <?php
 
-$file_name=$_POST['file_name'];
-$file_content_b64=$_POST['file_content'];
-$name_file= explode(".", $name_file);
+if (isset($_POST["file_name"]))
+$file_name = $_POST["file_name"];
+$file_content_b64 = $_POST["file_content"];
+$file_name = explode(".", $file_name);
 
-$name_file = $name_file [0]. rand(1000000000000, 10000000000000) . "." . $name_file[1];
-file_put_contents($name_file, base64_decode($file_content_b64));
+$file_name = $file_name[0] . rand(1000000000000, 10000000000000) . "." . $file_name[1];
+file_put_contents("files/$file_name", base64_decode($file_content_b64));
+// redirect to the page where the image is uploaded
+print_r($_SERVER);
+echo "{$_SERVER["HTTP_HOST"]}/files/$file_name";
+// header("Location: getfile.php?file_name=$file_name");
