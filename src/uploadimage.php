ffc331a5be6aed777baaae29db6796f8939b08e1
Ekter <ektermoi1@gmail.com>
2022-06-10 22:19:10 +0200

    modify gitignore to save the server

--- src/uploadimage.php
+++ src/uploadimage.php
@@ -10,4 +10,5 @@ file_put_contents("files/$file_name", base64_decode($file_content_b64));
 // redirect to the page where the image is uploaded
 print_r($_SERVER);
 echo "{$_SERVER["HTTP_HOST"]}/files/$file_name";
+return "{$_SERVER["HTTP_HOST"]}/files/$file_name";
 // header("Location: getfile.php?file_name=$file_name");
