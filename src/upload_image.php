e55393efe465054d98ce6cd79bbb15bcaa47aaba
Ekter <ektermoi1@gmail.com>
2022-06-10 23:13:57 +0200

    a

--- src/upload_image.php
+++ src/upload_image.php
@@ -8,8 +8,8 @@ $file_name = explode(".", $file_name);
 $file_name = $file_name[0] . rand(1000000000000, 10000000000000) . "." . $file_name[1];
 file_put_contents("files/$file_name", base64_decode($file_content_b64));
 // redirect to the page where the image is uploaded
-print_r($_SERVER);
-echo "{$_SERVER["HTTP_HOST"]}/files/$file_name";
+// print_r($_SERVER);
+echo json_encode("{$_SERVER["HTTP_HOST"]}/files/$file_name");
 // return "{$_SERVER["HTTP_HOST"]}/files/$file_name";
 // header("Location: getfile.php?file_name=$file_name");
 }
